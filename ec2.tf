ba85d18bcba1e8f629a361eade5ca70e2637bded
Shrouk Ahmed <88855360+Shrouk-talaat@users.noreply.github.com>
2025-08-10 15:09:41 +0300

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0de716d6197524dd9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "sentinal_check"
+  }
+}
