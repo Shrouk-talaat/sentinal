9582847b7a697314f08d4fd9865f4a20fad192a3
Shrouk Ahmed <88855360+Shrouk-talaat@users.noreply.github.com>
2025-08-10 16:05:02 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami           = "ami-0de716d6197524dd9"
   instance_type = "t2.micro"
+  subnet_id     = "subnet-09239666bd3a4ceaf"
 
   tags = {
     Name = "sentinal_check"
